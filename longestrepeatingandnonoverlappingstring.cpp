78df340d02874613f17b75e5808012b48cf053ea
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-03-07 19:08:04 +0530

    Add files via upload

---
+++ longestrepeatingandnonoverlappingstring.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    string longestSubstring(string s, int n) {
+   
+        
+        string ans = "-1";
+        for(int i = 0, j = 0; j<n; ++j){
+            string t = s.substr(i,j-i+1);
+            if(s.find(t,j+1)!=-1)   ans = t;
+            else    ++i;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        string S;
+
+        cin >> N;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.longestSubstring(S, N) << endl;
+    }
+    return 0;
+}
