0a755e602f007b8a0dd93c835a2473e2f74bd1b8
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-04-28 22:47:59 +0530

    Add files via upload

---
+++ DeleteMiddleOfLinkedList.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+x
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+  
+           if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+        Node* temp = head;
+        unordered_map<int, Node*> mp;
+        int n = 0;
+        while(temp != NULL){
+            n++;
+            mp[n] = temp;
+            temp = temp->next;
+        }
+        n = n/2;
+        mp[n]->next = mp[n+2]; 
+        return head;
+    }
+};
+
+
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		Solution obj;
+		head = obj.deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
