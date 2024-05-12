f00bcb975f0d3f0d65d8a27db76cfd387739f11e
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-05-12 15:35:03 +0530

    Add files via upload

---
+++ MinimumStepsToDestination.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+  public:
+    int minSteps(int d) {
+        int sum=0,steps=0,i=0;
+        while(sum<d){
+            sum+=steps;
+            steps++;
+        }
+        while((sum-d)%2==1){
+            sum+=steps;
+            steps++;
+        }
+       
+        
+      return steps-1;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
