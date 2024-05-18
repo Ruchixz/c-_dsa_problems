6164326058c30c7f5f607513e5f320467882afe8
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-05-18 20:46:17 +0530

    Add files via upload

---
+++ FindHighest.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+         int s = 0;
+        int n = a.size();
+        int e = a.size() - 1;
+        
+        int maxi = INT_MIN;
+        while(s <= e){
+            int mid = (s+e)/2;
+            
+            if(mid+1 < n && a[mid] < a[mid+1]){
+                maxi = max(maxi,a[mid+1]);
+                s = mid+1;
+            }
+            else if(mid-1 >= 0 && a[mid-1] > a[mid]){
+                maxi = max(maxi,a[mid-1]);
+                e = mid-1;
+            }
+            else{
+                maxi = max(maxi,a[mid]);
+                break;
+            }
+            
+        }
+        return maxi;
+        
+    }
+};
+
+
+
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
