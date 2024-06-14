8fc464afc81f3b9887a67ce3a8e47a81b4f4d760
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-06-14 11:09:50 +0530

    Add files via upload

---
+++ ArmstrongNumber.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        int sum=0;
+        int temp=n;
+        while(temp){
+            int r=temp%10;
+            sum+=r*r*r;
+            temp/=10;
+        }
+        return sum==n? "Yes":"No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
