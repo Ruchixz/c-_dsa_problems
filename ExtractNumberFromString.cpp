e08c736874c3df3fc173cada4d833edea2042618
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-06-22 23:14:29 +0530

    Add files via upload

---
+++ ExtractNumberFromString.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+      long long ExtractNumber(string str) {
+
+        string _maxi;
+        long long maxi = -1, temp;
+        
+        int i = 0, j = 0;
+        
+        while(i < str.size()){
+            
+            bool t = 0;
+            
+            if(str[i] >= '0' && str[i] <= '9'){
+                
+                while(i < str.size() && (str[i] >= '0' && str[i] <= '9')){
+                    
+                    if(str[i] == '9')   t = 1;
+                    
+                    i++;
+                }
+                
+                if(!t){
+                    
+                    _maxi = str.substr(j, i-j);
+                    
+                    temp = stoll(_maxi);
+                    
+                    maxi = max(maxi, temp);
+                }
+            }
+            i++;
+            j = i;
+        }
+        
+        return maxi ;
+
+    }
+};
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+
+        Solution ob;
+        cout << ob.ExtractNumber(s) << "\n";
+    }
+
+    return 0;
+}
+
