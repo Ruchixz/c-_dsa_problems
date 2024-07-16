b99864072ae5081a248c7f8bf31b806681b200b4
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-07-16 19:14:10 +0530

    Add files via upload

---
+++ RemainingString.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        string ans="";
+        int i=0;
+        
+        for( i=0;i<s.size();i++){
+            
+            if(s[i]==ch&&count){
+                count--;
+            }
+            if(count==0) break;
+        }
+        
+        if(count==0){
+            for(int j=i+1;j<s.size();j++){
+                ans+=s[j];
+            }
+            return ans;
+        }
+        
+        return "";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
