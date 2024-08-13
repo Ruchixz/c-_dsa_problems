88e44e7c0e3f2fab20b18acb18568a098842f601
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-08-13 18:38:29 +0530

    Add files via upload

---
+++ SquareRoot.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    long long int floorSqrt(long long int n) {
+        return floor(pow(n, 0.5));
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        Solution obj;
+        cout << obj.floorSqrt(n) << endl;
+    }
+    return 0;
+}
