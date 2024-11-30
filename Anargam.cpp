7c173a7562a7c34c8c58d8e9c684134189f858df
Ruchixz <138874960+Ruchixz@users.noreply.github.com>
2024-11-30 20:44:13 +0530

    Add files via upload

---
+++ Anargam.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+       if(s1.length()!=s2.length()) return false;
+       vector<int> freq(26);
+       for(int i=0; i<s1.length();i++){
+           freq[s1[i]-'a']++;
+           freq[s2[i]-'a']--;
+       }
+       return freq== vector<int>(26);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
